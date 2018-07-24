4801ebf7337942591e4d7f89fa16bf8dc7d6a93b
Dwight Townsend <townsenddw@gmail.com>
2018-07-24 11:27:36 -0700

    cluster autoscaler to 0

--- deploy-cas.sh
+++ deploy-cas.sh
@@ -5,4 +5,4 @@ helm repo update
 helm install stable/cluster-autoscaler \
   --name cluster-autoscaler \
   --set awsRegion=us-west-2 \
-  --set "autoscalingGroups[0].name=spot.calpolydatascience.k8s.local,autoscalingGroups[0].maxSize=3,autoscalingGroups[0].minSize=1"
+  --set "autoscalingGroups[0].name=spot.calpolydatascience.k8s.local,autoscalingGroups[0].maxSize=3,autoscalingGroups[0].minSize=0"
